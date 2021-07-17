ddef54cb28fb55a6f6e922e82ec68c51cb55fcdd
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-07-17 15:46:33 -0300

    creating / join in rooms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState, useEffect } from "react";
-
-import { createContext } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { firebase, auth } from "./services/firebase";
+import { Room } from "./pages/Room";
 
 import "./styles/global.scss";
 
@@ -14,8 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
+        <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/rooms/new" component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
